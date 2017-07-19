79e6788b5cf652090b5652caa1aed60dac9adaca
mimilettd <mimilettd@gmail.com>
2017-07-19 09:25:12 -0600

    complete iteration 1

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,26 @@
 class Joke
+  attr_reader :id,
+              :question,
+              :answer
+
+  def initialize(hash={})
+    @id = hash[:id]
+    @question = hash[:question]
+    @answer = hash[:answer]
+  end
+
+
+  def id
+    @id
+  end
+
+  def question
+    @question
+  end
+
+  def answers
+    @answer
+  end
+
 
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -4,5 +4,28 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
+  def test_class_exist
+    joke = Joke.new
+    assert_instance_of Joke, joke
+  end
 
+  def test_instance_of_new_class
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_instance_of Joke, joke
+  end
+
+  def test_joke_id
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_equal 1, joke.id
+  end
+
+  def test_joke_question
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_equal "Why did the strawberry cross the road?", joke.question
+  end
+
+  def test_joke_answers
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_equal "Because his mother was in a jam.", joke.answers
+  end
 end
